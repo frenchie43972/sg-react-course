659ace8773512f88af69ecd8df0199b2ad673b6e
French <1078924850117002@mil>
2023-12-07 07:27:22 +0900

    local

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import { useState } from "react";
+import axios from "axios";
 import BookCreate from './components/BookCreate';
 import BookList from "./components/BookList";
 
 function App() {
   const [books, setBooks] = useState([]);
 
-  const createBook = (title) => { 
-    const createdBooks = [
-      ...books,
-      {
-        id: Math.round(Math.random() * 999),
-        title
-      }
-    ];
-    setBooks(createdBooks)
+  const createBook = async (title) => { 
+    // const createdBooks = [
+    //   ...books,
+    //   {
+    //     id: Math.round(Math.random() * 999),
+    //     title
+    //   }
+    // ];
+    // setBooks(createdBooks)
+    const response = await axios.post('http://localhost:3001/books', {
+      title,
+    });
+
+    console.log(response)
   };
 
   const deleteBookById = (id) => { 
